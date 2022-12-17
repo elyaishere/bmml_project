14907ad526846c9bcf8c4750e8ab45c41876ff03
Olga Gorbunova <elyaishere@mail.ru>
2022-12-17 21:55:46 +0300

    fix

--- code/fgsm.py
+++ code/fgsm.py
@@ -2,10 +2,10 @@ import torch
 
 
 class FGSM(torch.optim.Optimizer):
-    def __init__(self, params, base_optimizer, rho=0.05, **kwargs):
+    def __init__(self, params, base_optimizer, rho=0.05, eps=0.001, **kwargs):
         assert rho >= 0.0
 
-        defaults = dict(rho=rho, **kwargs)
+        defaults = dict(rho=rho, eps=eps, **kwargs)
         super(FGSM, self).__init__(params, defaults)
 
         self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
@@ -16,12 +16,14 @@ class FGSM(torch.optim.Optimizer):
     def first_step(self, zero_grad=False, save_state=False):
         for group in self.param_groups:
             scale = group["rho"]
+            eps = group["eps"]
 
             for p in group["params"]:
                 if p.grad is None: continue
                 if save_state: self.state[p]["old_p"] = p.data.clone()
                 e_w = scale * torch.sign(p.grad)
                 p.add_(e_w)
+                p = torch.max(torch.min(p, self.state[p]["old_p"] + eps), self.state[p]["old_p"] - eps)
 
         if zero_grad: self.zero_grad()
 
