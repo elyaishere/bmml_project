b8439f79fdc1eb9db7ae00c00883cbf57589b04f
Olga Gorbunova <elyaishere@mail.ru>
2022-12-17 14:27:37 +0300

    init

--- code/utils.py
+++ code/utils.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from torch.nn.modules.batchnorm import _BatchNorm
 from IPython.display import clear_output
 import matplotlib.pyplot as plt
@@ -40,3 +41,12 @@ def create_checkoint_dir(root, model_name, dataset_name):
     checkpoint_dir = os.path.join(root, model_name, dataset_name)
     if not os.path.exists(checkpoint_dir):
         os.makedirs(checkpoint_dir)
+
+
+def top_k_error_rate_metric(logits, targets, k):
+  mask = np.ones([logits.shape[0]])
+  true_labels = targets.reshape(-1, 1)
+  top_k_preds = np.argsort(logits, axis=-1)[:, -k:]
+  hit = np.array([i in top for i, top in zip(true_labels, top_k_preds)])
+  error_rate = 1 - ((hit * mask).sum() / mask.sum())
+  return error_rate
